be1b2d6f520a7c1168291b481d6385c9c4ef7541
andrey-vasenin <vaseninand@yandex.ru>
2023-05-17 11:03:54 +0300

    Relaxation rate for the second qubit is now 2 * g2; function for computing coherent and incoherent powers

--- main.cpp
+++ main.cpp
@@ -23,6 +23,10 @@ typedef std::vector<val_t> tlist_t;
 typedef std::vector<cmpx> peaks_t;
 typedef std::tuple<val_t, val_t, val_t, val_t, val_t, val_t, val_t> params_t;
 typedef std::tuple<peaks_t, peaks_t> result_t;
+typedef std::vector<val_t> zlist_t;
+typedef std::vector<val_t> state2_t;
+typedef std::vector<std::vector<cmpx>> op_t;
+typedef std::vector<val_t> map_t;
 
 struct push_back_state_and_time
 {
@@ -53,7 +57,7 @@ struct push_back_state_and_time2
 
 	void operator()(const state_t& x, val_t t)
 	{
-		if (t > t_init_to_skip)
+		if (t >= t_init_to_skip)
 		{
 			int N = q1.size();
 			for (int i = 0; i < N; i++)
@@ -62,6 +66,46 @@ struct push_back_state_and_time2
 				q1[i] += cmpx(x[0], (-1.f) * x[1]) * std::exp(cmpx(0.f, delta * t * static_cast<float>(order)));
 				q2[i] += cmpx(x[3], (-1.f) * x[4]) * std::exp(cmpx(0.f, delta * t * static_cast<float>(order)));
 			}
+		}
+	}
+};
+
+struct push_back_state_and_time3
+{
+	std::vector<state2_t>& m_states;
+	tlist_t& m_times;
+
+	push_back_state_and_time3(std::vector<state2_t>& states, tlist_t& times)
+		: m_states(states), m_times(times) { }
+
+	void operator()(const state_t& x, val_t t)
+	{
+		m_states.push_back(state2_t(x.begin(), x.end()));
+		m_times.push_back(t);
+	}
+};
+
+struct compute_coherent_and_incoherent_energy
+{
+	val_t coh_en, incoh_en;
+	val_t g1, g2, g1sqrt, g2sqrt;
+	val_t t_init_to_skip;
+	val_t t0;
+
+	compute_coherent_and_incoherent_energy(val_t g1_, val_t g2_, val_t t_init_to_skip)
+		: coh_en(0), incoh_en(0), g1(g1_), g2(g2_), g1sqrt(std::sqrtf(g1)), g2sqrt(std::sqrtf(g2)),
+		  t_init_to_skip(t_init_to_skip), t0(t_init_to_skip) {}
+
+	void operator()(const state_t& x, val_t t)
+	{
+		if (t > t_init_to_skip)
+		{
+			val_t dt = t - t0;
+			t0 = t;
+			val_t sx = g1sqrt * x[0] + g2sqrt * x[3];
+			val_t sy = g1sqrt * x[1] + g2sqrt * x[4];
+			coh_en += (sx * sx + sy * sy) * 0.25f * dt;
+			incoh_en += (g1 * (1.f + x[2]) + g2 * (1.f + x[5]) + g1sqrt * g2sqrt * (x[6] + x[10])) * 0.5f * dt;
 		}
 	}
 };
@@ -82,7 +126,7 @@ public:
 		t_init_to_skip(t_init_to_skip)
 	{}
 
-	std::tuple<tlist_t, trace_t, trace_t> rosenbrock_solve(const std::tuple<val_t, val_t> &amplitudes)
+	std::tuple<tlist_t, std::vector<state2_t>> rosenbrock_solve(const std::tuple<val_t, val_t> &amplitudes)
 	{
 		using namespace boost::numeric::odeint;
 		Eq eq(std::get<0>(amplitudes), std::get<1>(amplitudes), eta1, g1, g2, alpha, domega, eps1, eps2);
@@ -93,18 +137,16 @@ public:
 
 		int total_steps = static_cast<int>(roundf((t1 - t0) / dt));
 
-		trace_t sigmam1;
-		trace_t sigmam2;
 		tlist_t times;
-		sigmam1.reserve(total_steps);
-		sigmam2.reserve(total_steps);
 		times.reserve(total_steps);
+		std::vector<state2_t> states;
+		states.reserve(total_steps);
 
 		rosenbrock4<val_t> rb4stepper;
 		integrate_const(rb4stepper, make_pair(eq, eq.system), x, t0, t1, dt,
-			push_back_state_and_time(sigmam1, sigmam2, times));
+			push_back_state_and_time3(states, times));
 
-		return std::make_tuple(times, sigmam1, sigmam2);
+		return std::make_tuple(times, states);
 	}
 
 	result_t rosenbrock_solve2(const std::tuple<val_t, val_t>& amplitudes)
@@ -126,13 +168,35 @@ public:
 
 		return std::make_tuple(qubit1, qubit2);
 	}
+	
+	std::tuple<val_t, val_t> solve_for_power(const std::tuple<val_t, val_t>& amplitudes)
+	{
+		using namespace boost::numeric::odeint;
+		Eq eq(std::get<0>(amplitudes), std::get<1>(amplitudes), eta1, g1, g2, alpha, domega, eps1, eps2);
+
+		state_t x(N, 0.f);
+		x[2] = -1.f;
+		x[5] = -1.f;
+		x[14] = 1.f;
+
+		peaks_t qubit1(8, cmpx(0.f));
+		peaks_t qubit2(8, cmpx(0.f));
+
+		compute_coherent_and_incoherent_energy computer(g1, g2, t_init_to_skip);
+
+		rosenbrock4<val_t> rb4stepper;
+		integrate_const(rb4stepper, make_pair(eq, eq.system), x, t0, t1, dt,
+			push_back_state_and_time2(qubit1, qubit2, domega, t_init_to_skip));
+
+		return std::make_tuple(computer.coh_en, computer.incoh_en);
+	}
 
 	result_t operator()(const std::tuple<val_t, val_t> &amplitudes)
 	{
 		return rosenbrock_solve2(amplitudes);
 	}
 
-	std::tuple<tlist_t, trace_t, trace_t> rk45_solve(const std::tuple<val_t, val_t> &amplitudes)
+	std::tuple<tlist_t, std::vector<state2_t>> rk45_solve(const std::tuple<val_t, val_t> &amplitudes)
 	{
 		using namespace boost::numeric::odeint;
 		Eq eq(std::get<0>(amplitudes), std::get<1>(amplitudes), eta1, g1, g2, alpha, domega, eps1, eps2);
@@ -143,16 +207,15 @@ public:
 
 		int total_steps = static_cast<int>(roundf((t1 - t0) / dt));
 
-		trace_t sigmam1;
-		trace_t sigmam2;
 		tlist_t times;
-		sigmam1.reserve(total_steps);
-		sigmam2.reserve(total_steps);
 		times.reserve(total_steps);
+		std::vector<state2_t> states;
+		states.reserve(total_steps);
 
 		//runge_kutta4_classic<state_t, val_t> stepper;
-		integrate(eq, x, t0, t1, dt, push_back_state_and_time(sigmam1, sigmam2, times));
-		return { times, sigmam1, sigmam2 };
+		integrate(eq, x, t0, t1, dt,
+			push_back_state_and_time3(states, times));
+		return std::make_tuple(times, states);
 	}
 
 
@@ -196,6 +259,37 @@ std::vector<result_t> qwm_parallel_solve(params_t params, val_t T, val_t time_st
 	return results;
 }
 
+std::tuple<map_t, map_t> coherent_and_incoherent_powers(params_t params, val_t T, val_t time_step,
+	std::vector<val_t> W_amplitudes, std::vector<val_t> E_amplitudes,
+	val_t t_init_to_skip)
+{
+	qwm_solver qwmsolver(params, T, time_step, t_init_to_skip);
+	std::vector<std::tuple<val_t, val_t>> amplitudes;
+	for (val_t& wamp : W_amplitudes)
+		for (val_t& eamp : E_amplitudes)
+			amplitudes.push_back({ wamp, eamp });
+	map_t coherent_energy(amplitudes.size());
+	map_t incoherent_energy(amplitudes.size());
+
+	struct Transform {
+		void operator()(const tbb::blocked_range<size_t>& r) const {
+			for (size_t i = r.begin(); i != r.end(); ++i) {
+				auto res = solver.solve_for_power(ampls[i]);
+				coh_en[i] = std::get<0>(res);
+				incoh_en[i] = std::get<1>(res);
+			}
+		}
+		std::vector<std::tuple<val_t, val_t>>& ampls;
+		map_t& coh_en;
+		map_t& incoh_en;
+		qwm_solver& solver;
+	} transform{ amplitudes, coherent_energy, incoherent_energy, qwmsolver };
+
+	tbb::parallel_for(tbb::blocked_range<size_t>(0, amplitudes.size()), transform);
+
+	return {coherent_energy, incoherent_energy};
+}
+
 PYBIND11_MODULE(QWM, m) {
 	m.doc() = "Quantum Wave Mixing Solver"; // optional module docstring
 	py::class_<qwm_solver>(m, "QWMSolver")
@@ -207,6 +301,7 @@ PYBIND11_MODULE(QWM, m) {
 		.def("rosenbrock_solve_multiple", &qwm_solver::rosenbrock_solve_multiple,
 			"Takes an array of W amplitudes and an array of E amplitudes and solves qwm system of equations on a grid of these amplitudes");
 	m.def("qwm_parallel_solve", &qwm_parallel_solve);
+	m.def("qwm_total_power", &coherent_and_incoherent_powers);
 }
 //void dump_in_file(const std::vector<result_t>& data)
 //{
